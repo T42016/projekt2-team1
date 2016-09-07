ba59d41c34c78ca8ab3e89824fc7b05fad59e886
KiaShiMa <kiasar.mian@lund.ntig.se>
2016-09-07 09:21:34 +0200

    15 correct

--- MineSweeper/MineSweeperLogic/MineSweeperGame.cs
+++ MineSweeper/MineSweeperLogic/MineSweeperGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
+using System.Net.Mime;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,8 @@ namespace MineSweeperLogic
             SizeX = sizeX;
             SizeY = sizeY;
             NumberOfMines = nrOfMines;
+            State = State;
+            Bus = bus;
         }
 
         public int PosX { get; private set; }
@@ -24,13 +27,12 @@ namespace MineSweeperLogic
         public int SizeY { get; }
         public int NumberOfMines { get; }
         public GameState State { get; private set; }
+        public IServiceBus Bus { get; }
+        ServiceBus sB = new ServiceBus();
 
         public PositionInfo GetCoordinate(int x, int y)
         {
-            if (PosY >= SizeY)
-            {
-                
-            }
+            
             return null;
 
 
@@ -47,11 +49,32 @@ namespace MineSweeperLogic
 
         public void ResetBoard()
         {
+
         }
 
         public void DrawBoard()
         {
-            
+            for (int i = 0; i < SizeY; i++)
+            {
+                for (int j = 0; j < SizeX; j++)
+                {
+
+
+                    if (j == PosX && i == PosY)
+                    {
+                        sB.Write("O ", ConsoleColor.Red);
+
+                    }
+                    else
+                    {
+                        sB.Write(". ", ConsoleColor.Blue);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            Bus.Write("! ");
+
         }
 
         #region MoveCursor Methods
