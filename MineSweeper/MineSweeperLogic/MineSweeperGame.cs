cd1a2c838b3dab768482aca1812542264c211e1c
KiaShiMa <kiasar.mian@lund.ntig.se>
2016-09-07 08:57:23 +0200

    14 passed

--- MineSweeper/MineSweeperLogic/MineSweeperGame.cs
+++ MineSweeper/MineSweeperLogic/MineSweeperGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +10,12 @@ namespace MineSweeperLogic
 {
     public class MineSweeperGame
     {
-
+        
         public MineSweeperGame(int sizeX, int sizeY, int nrOfMines, IServiceBus bus)
         {
+            SizeX = sizeX;
+            SizeY = sizeY;
+            NumberOfMines = nrOfMines;
         }
 
         public int PosX { get; private set; }
@@ -23,7 +27,13 @@ namespace MineSweeperLogic
 
         public PositionInfo GetCoordinate(int x, int y)
         {
+            if (PosY >= SizeY)
+            {
+                
+            }
             return null;
+
+
         }
 
         public void FlagCoordinate()
@@ -32,6 +42,7 @@ namespace MineSweeperLogic
 
         public void ClickCoordinate()
         {
+            
         }
 
         public void ResetBoard()
@@ -40,27 +51,42 @@ namespace MineSweeperLogic
 
         public void DrawBoard()
         {
+            
         }
 
         #region MoveCursor Methods
 
         public void MoveCursorUp()
         {
+            if (PosY > 0)
+            {
+                PosY--;
+            }
         }
 
         public void MoveCursorDown()
         {
+            if(PosY < SizeY -1)
+            PosY++;
         }
 
         public void MoveCursorLeft()
         {
+            if (PosX > 0)
+            {
+                PosX--;
+            }
         }
 
         public void MoveCursorRight()
         {
+            if(PosX +1  < SizeX)
+                PosX++;
         }
 
         #endregion
 
+        #region CorrectPos
+        #endregion
     }
 }
